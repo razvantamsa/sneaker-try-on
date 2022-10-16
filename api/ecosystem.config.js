6933654b7ace286e6ffd3bad93ea8b378cb8bfa5
ραρεσ <57955143+StoicaRares14@users.noreply.github.com>
2022-10-16 13:45:20 +0300

    Set up api

---
+++ api/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [{
+      name: "api",
+      script: "dist/main.js",
+      instances: "1",
+      exec_mode : "cluster",
+      node_args: "--max-old-space-size=512 --nouse-idle-notification",
+      args: "--max-old-space-size=512 --nouse-idle-notification"
+    }],
+  };
+  

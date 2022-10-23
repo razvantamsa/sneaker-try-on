74f4e1f1915bb8efc2f2e7675c6fae1318f9bf03
Razvan Tamsa <razvan.tamsa@opswat.com>
2022-10-23 14:30:53 +0300

    rename root to backend

--- backend/apps/image-processing/test/app.e2e-spec.ts
+++ backend/apps/image-processing/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AuthModule } from './../src/auth.module';
+import { ImageProcessingModule } from '../src/image-processing.module';
 
-describe('AuthController (e2e)', () => {
+describe('ImageProcessingController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule],
+      imports: [ImageProcessingModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();

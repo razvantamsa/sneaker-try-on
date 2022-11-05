7264aadbadc831d1c9e124aebba0182c48e7f87a
ραρεσ <57955143+StoicaRares14@users.noreply.github.com>
2022-11-05 13:54:47 +0200

    Add models

---
+++ backend/apps/sneaker/src/models/brand.model.ts
@@ -0,0 +1,46 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import { SneakerModel } from './sneaker.model';
+
+@ObjectType()
+@Entity({ name: 'brand' })
+export class BrandModel {
+  @Field({ nullable: false })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field({ nullable: true })
+  @Column('varchar', { length: 255, name: 'name', nullable: true })
+  name?: string;
+
+  @Field({ nullable: false })
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @Field({ nullable: false })
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+
+  @Field(() => SneakerModel, { nullable: true })
+  @ManyToOne(() => SneakerModel, (sneaker) => sneaker.brand, {
+    cascade: true,
+  })
+  sneaker: SneakerModel;
+}

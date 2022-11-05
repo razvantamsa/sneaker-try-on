aa3d45e17542145dad88c7b6afcef3428c744509
ραρεσ <57955143+StoicaRares14@users.noreply.github.com>
2022-11-05 13:53:23 +0200

    Create sneaker microservice

--- backend/apps/sneaker/src/main.ts
+++ backend/apps/sneaker/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+
 import { SneakerModule } from './sneaker.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(SneakerModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(SneakerModule, {
+    transport: Transport.TCP,
+    options: { port: 3002 },
+  });
+  await app.listen();
 }
 bootstrap();

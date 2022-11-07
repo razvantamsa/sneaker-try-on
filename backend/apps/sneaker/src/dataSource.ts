95d8ffba20c41abf560ccb9ddb5316ca68806464
ραρεσ <57955143+StoicaRares14@users.noreply.github.com>
2022-11-07 10:57:16 +0200

    Delete old sneaker implementation

--- backend/apps/sneaker/src/dataSource.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from 'typeorm';
-
-export const SneakerDataSource = new DataSource({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  logging: true,
-  entities: ['dist/models/*.js'],
-  migrations: ['dist/migrations/*.js'],
-});
--- backend/apps/sneaker/src/interfaces/updateSneaker.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateSneakerDto {
-  @Field({ nullable: true })
-  name?: string;
-}

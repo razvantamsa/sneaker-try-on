052b07c259bdfbf5631d6fdedd410b0fa3dbceed
razvantamsa <razvantamsa295@gmail.com>
2022-12-20 18:44:57 +0200

    init with typescrpt

--- flashcard-frontend/App.tsx
+++ flashcard-frontend/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Home boy doin mobile</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
